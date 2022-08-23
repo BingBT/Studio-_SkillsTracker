26af95a45cc1b5ff3ca06517caf594a377f6df01
BingbingT <lerimai0525@gmail.com>
2022-08-23 00:17:30 -0400

    finished studio 10

---
+++ src/main/java/org/launchcode/Studio_SkillsTracker/StudioSkillsTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Studio_SkillsTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioSkillsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudioSkillsTrackerApplication.class, args);
+	}
+
+}
